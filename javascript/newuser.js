012cef4851151d2bae063c679640832297d335ec
ishergill <90960009+ishergill@users.noreply.github.com>
2021-11-20 10:16:50 +0530

    Add files via upload

---
+++ javascript/newuser.js
@@ -0,0 +1,65 @@
+// phone no veryfication
+    const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
+
+    const info = document.querySelector(".alert-info");
+    const error = document.querySelector(".alert-error");
+
+    function process(event) {
+      event.preventDefault();
+
+      const phoneNumber = phoneInput.getNumber();
+
+      info.style.display = "";
+      info.innerHTML = `Phone number in E.164 format: <strong>${phoneNumber}</strong>`;
+    }
+
+// otp verification
+$(function() {
+    'use strict';
+  
+    var body = $('body');
+  
+    function goToNextInput(e) {
+      var key = e.which,
+        t = $(e.target),
+        sib = t.next('input');
+  
+      if (key != 9 && (key < 48 || key > 57)) {
+        e.preventDefault();
+        return false;
+      }
+  
+      if (key === 9) {
+        return true;
+      }
+  
+      if (!sib || !sib.length) {
+        sib = body.find('input').eq(0);
+      }
+      sib.select().focus();
+    }
+  
+    function onKeyDown(e) {
+      var key = e.which;
+  
+      if (key === 9 || (key >= 48 && key <= 57)) {
+        return true;
+      }
+  
+      e.preventDefault();
+      return false;
+    }
+    
+    function onFocus(e) {
+      $(e.target).select();
+    }
+  
+    body.on('keyup', 'input', goToNextInput);
+    body.on('keydown', 'input', onKeyDown);
+    body.on('click', 'input', onFocus);
+  
+  })
